d845add3f13a16830873772e8a0f9b208898d848
tithi23 <tithiratry@gmail.com>
2021-10-19 08:28:34 -0700

    privateroute and auth related problem

---
+++ src/components/Login/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+// import useFirebase from '../hooks/useFirebase';
+
+
+const Login = () => {
+    const { signInUsingGoogle } = useAuth();
+    return (
+        <div>
+            <h2>Please Login</h2>
+            <button onClick={signInUsingGoogle}>Google sign in</button>
+            <br />
+            <Link to="/register">New user?</Link>
+        </div>
+    )
+}
+
+
+export default Login;
